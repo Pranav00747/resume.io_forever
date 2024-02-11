0a10f6755c44d17e6329e10d646526468c867807
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2024-02-11 09:36:43 +0530

    Delete lastly_forever.clj

--- lastly_forever.clj
+++
@@ -1,3 +0,0 @@
-(defn lastly_notes[](
-   ;; Please protect me because I don't wanna lose that things after no freedom, Please protect us I want security
-   ;; I love my dad & mom and my brother mostly but i don't know whether they love me or not and I want protection at all Z-security
